a73cf3dd91da9e705e93cd3546f98482ba074023
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-06-17 20:57:54 +0900

    Add: Model Specの学習用に作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   enum status: { todo: 0, doing: 1, done: 2 }
-  validates :title, presence: true
+  # validates :title, presence: true
   validates :status, presence: true
   validates :deadline, presence: true
   validate :deadline, :must_start_from_today
